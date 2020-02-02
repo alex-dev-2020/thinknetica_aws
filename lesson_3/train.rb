5bc05d97555d7ae09bc8f94564a9804e0c0d437a
alexfmjpn <lexfnjpm@mail.ru>
2020-02-02 23:22:21 +0000

    Created Train class structure

---
+++ lesson_3/train.rb
@@ -0,0 +1,54 @@
+case Train
+
+  def initialize
+# id (number), type (pass/cargo), vagon (q-ty)
+  
+  
+  end
+  
+# speed up
+  def
+  
+  end
+  
+# slow down
+  def
+  
+  end
+  
+# vagon quantity
+ def
+ 
+ 
+ end
+
+# add/detach vagons (one per action, only if speed = 0)
+ def
+ 
+ end
+ 
+# accept route
+ def
+ 
+ end
+ 
+# if accep route - train placed at 1st station of route !!!
+
+ def
+ 
+ end
+
+# moves among  stations in route (one shift per action) 
+
+  def
+  
+  end
+  
+# return station name (previous current, next)
+
+  def
+  
+  end
+
+    
+end
