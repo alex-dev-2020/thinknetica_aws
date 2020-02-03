33886bec6bd3219160a227c48a964647158f2176
alexfmjpn <lexfnjpm@mail.ru>
2020-02-03 07:13:10 +0000

    Added some methds to train

--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -1,35 +1,50 @@
 class Train
  
-  attr_reader  :
+  attr_reader  :train_id, :train_type :wagons_count
   
-  def initialize()
+  def initialize(train_id, type = "cargo", wagons_count_count = 10)
+    @train_id = train_id
+	@speed = 0
+	
+	@wagons_count_count = []
   
-# id (number), type (pass/cargo), vagon (q-ty)
+# id (number), type (pass/cargo), vagon_count (q-ty)
   
   
   end
   
 # speed up
-  def
-  
+  def speed_up(n)
+    @speed += n 
   end
   
-# slow down
-  def
-  
+# speed down
+  def speed_down(n)
+    @speed -= n
+  end
+# stop
+  def stop
+    @speed = 0  
   end
   
-# vagon quantity
+# add wagons (one per action, only if speed = 0)
+ def add_wagon
+   @wagons << wagon if speed == 0
+ end
+ 
+# detach wagons (one per action, only if speed = 0)
  def
+ 
+ end
+ 
+# wagons quantity
+ def wagons_count
+   
+   @wagons.length
  
  
  end
 
-# add/detach vagons (one per action, only if speed = 0)
- def
- 
- end
- 
 # accept route
  def
  
