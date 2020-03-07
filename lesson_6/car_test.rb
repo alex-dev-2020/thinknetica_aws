96f76df688adbef5f74356d4d1412dbbe3224bdc
alexfmjpn <lexfnjpm@mail.ru>
2020-03-07 15:40:24 +0000

    Added Class's  mthd via self.  statement

--- lesson_6/car_test.rb
+++ lesson_6/car_test.rb
@@ -1,2 +1,32 @@
  # this file for test purpose only
- 
+ class Car
+     attr_reader :current_rpm
+     
+     def self.description
+      puts "Это баззовый класс для всех авто"
+     end
+     
+     def initialize
+         @current_rpm = 0
+     end
+    
+     def start_engine
+         start_engine! if engine_stoppped?
+     end
+     
+     def engine_stoppped?
+      current_rpm.zero?
+     end
+     
+     protected
+     
+     attr_writer :current_rpm
+     
+     def initial_rpm
+      700
+     end
+     
+     def start_engine!
+      self.current_rpm = initial_rpm
+     end
+end
