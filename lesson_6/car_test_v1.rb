bfe883aa060f677c823055a1111e2110d0d9c4da
alexfmjpn <alexfmjpn@mail.ru>
2020-03-10 13:58:50 +0000

    Created next car_test file for screencast explore

---
+++ lesson_6/car_test_v1.rb
@@ -0,0 +1,35 @@
+ # this file for test purpose only
+ class Car
+     attr_reader :current_rpm
+     
+     def self.description
+      puts "Это базовый класс для всех авто"
+     end
+     
+     def description
+      puts "Это экземпляр класса Car"
+     end
+     
+     def initialize
+         @current_rpm = 0
+     end
+    
+     def start_engine
+         start_engine! if engine_stoppped?
+     end
+     
+     def engine_stoppped?
+      current_rpm.zero?
+     end
+     
+     protected
+     attr_writer :current_rpm
+     
+     def initial_rpm
+      700
+     end
+     
+     def start_engine!
+      self.current_rpm = initial_rpm
+     end
+end
